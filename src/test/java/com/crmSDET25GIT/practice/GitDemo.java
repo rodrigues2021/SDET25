70fafe1f94c8d6ca5d068a46a88aa5dc2ed73e79
hp ssd <hp ssd@DESKTOP-NAPD9NM>
2021-12-09 16:26:35 +0530

    first commit

---
+++ src/test/java/com/crmSDET25GIT/practice/GitDemo.java
@@ -0,0 +1,10 @@
+package com.crmSDET25GIT.practice;
+
+import org.testng.annotations.Test;
+
+public class GitDemo {
+	@Test
+public static void main(String[] args) {
+	System.out.println("hellow world...!");
+}
+}
